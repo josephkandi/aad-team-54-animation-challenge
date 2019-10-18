705394f2a3e87bd66cdc08b3e553c841dd424147
appdevgenie <appdevgenie@gmail.com>
2019-10-18 08:56:20 +0200

    update DetailActivity java, add toolbar, back button and menu

--- app/src/main/java/com/alc/aad54/activities/DetailActivity.java
+++ app/src/main/java/com/alc/aad54/activities/DetailActivity.java
@@ -1,16 +1,100 @@
 package com.alc.aad54.activities;
 
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.alc.aad54.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    private TextView tvDate;
+    private TextView tvTempMax;
+    private TextView tvTempMin;
+    private TextView tvInfo;
+    private TextView tvHumidity;
+    private TextView tvPressure;
+    private TextView tvWind;
+    private TextView tvUV;
+    private ImageView ivIcon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        setupVariables();
+    }
+
+    private void setupVariables() {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        tvDate = findViewById(R.id.tvDetailDate);
+        tvTempMax = findViewById(R.id.tvDetailTempMax);
+        tvTempMin = findViewById(R.id.tvDetailTempMin);
+        tvInfo = findViewById(R.id.tvDetailInfo);
+        tvHumidity = findViewById(R.id.tvDetailHumidityInfo);
+        tvPressure = findViewById(R.id.tvDetailPressureInfo);
+        tvWind = findViewById(R.id.tvDetailWindSpeedInfo);
+        tvUV = findViewById(R.id.tvDetailUVInfo);
+
+        ivIcon = findViewById(R.id.ivDetailIcon);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.menu_detail_settings:
+                //TODO: add settings options eg. custom location, preferred units(metric or imperial)
+                return true;
+
+            case R.id.menu_detail_share:
+
+                String weatherInfo = "";
+                //TODO: create info text to be shared
+                shareDetails(weatherInfo);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void shareDetails(String weatherInfo) {
+
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        sharingIntent.putExtra(Intent.EXTRA_TEXT, weatherInfo);
+        startActivity(Intent.createChooser(sharingIntent, getString(R.string.app_name)));
     }
 }
