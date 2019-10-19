42d29ee6eebb127a644d1bfdb521629364b662c4
appdevgenie <appdevgenie@gmail.com>
2019-10-19 16:18:36 +0200

    add transition animation

--- app/src/main/java/com/alc/aad54/activities/DetailActivity.java
+++ app/src/main/java/com/alc/aad54/activities/DetailActivity.java
@@ -65,7 +65,7 @@ public class DetailActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                finish();
+                onBackPressed();
             }
         });
 
